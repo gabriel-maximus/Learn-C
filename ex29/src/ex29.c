2aa656279e921e066db4f7b28416e9ccef270483
gabriel-maximus <gabrielmaximus80@gmail.com>
2023-08-14 10:46:09 -0300

    exercicio de dynamic library

---
+++ ex29/src/ex29.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "dbg.h"
+#include "libex29.h"
+
+typedef int (*lib_function)(const char  *data);
+
+int main()
+{
+    print_a_message("Teste");
+}
---
+++ ex29/src/libex29.h
@@ -0,0 +1,10 @@
+#ifndef _libex29_h_
+#define _libex29_h_
+
+int print_a_message(const char *msg);
+
+int uppercase(const char *msg);
+
+int lowercase(const char *msg);
+
+#endif
